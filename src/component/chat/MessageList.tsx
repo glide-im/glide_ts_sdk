ac562ed770a997ddfa95b604f69c777666b82dc8
dengzii <dengzii@foxmail.com>
2022-04-13 18:25:00 +0800

    remove unused

--- src/component/chat/MessageList.tsx
+++ src/component/chat/MessageList.tsx
@@ -24,20 +24,20 @@ export function SessionMessageList(props: { id: number }) {
         }
     }, [session, messages])
 
-    const loadHistory = () => {
-        session?.getMessageHistry(0)
-            .subscribe({
-                next: (r) => {
-                    setMessages(r)
-                },
-                error: (e) => {
-                    console.log("getMessageHistry", e)
-                },
-                complete: () => {
-                    setLoading(false)
-                }
-            })
-    }
+    // const loadHistory = () => {
+    //     session?.getMessageHistry(0)
+    //         .subscribe({
+    //             next: (r) => {
+    //                 setMessages(r)
+    //             },
+    //             error: (e) => {
+    //                 console.log("getMessageHistry", e)
+    //             },
+    //             complete: () => {
+    //                 setLoading(false)
+    //             }
+    //         })
+    // }
 
     if (session == null) {
         return <Box mt={"50%"}>
